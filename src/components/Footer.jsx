7994cb4756ce899584195250408234f463c0ad4b
Nadjib-coder <nadjib.programmation1@gmail.com>
2025-04-10 14:39:37 +0100

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        © 2025{' '}
+        <a href="https://nadjibmouhoun.me/" target="_blank">
+          Nadjib Mouhoun{' '}
+        </a>
+        development. All rights reserved.
+      </p>
+    </footer>
+  );
+}
